f83a98c70c3a4bd690cba9b4a943dd76932077e8
hadbet <hadbet.altamirano@grammer.com>
2025-08-19 09:20:21 -0600

    01_Inicion de carga aplicacion web

--- inicio.php
+++ inicio.php
@@ -75,7 +75,7 @@
                 <!-- Responsible Person -->
                 <div>
                     <label for="responsable" class="block text-sm font-medium text-gray-700 mb-1">Responsable <span class="text-red-500">*</span></label>
-                    <input type="text" id="responsable" name="responsable" required class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus-ring-grammer">
+                    <input type="text" id="responsable" name="responsable" required class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus-ring-grammer" value="<?php echo $nombre;?>">
                 </div>
                 <!-- Fixed Asset (Optional) -->
                 <div>
